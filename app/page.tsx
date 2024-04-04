77ad7a06e5682db29243fc255e0749f7e31950ac
Joshua Melville <joshua.melville@northwestern.edu>
2024-04-04 13:30:15 +0200

    minify logo and add github link

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between gap-10">
-      <div className="grow shrink-0 flex justify-center items-center">
+      <div className="grow shrink-0 flex flex-col justify-center items-center">
         <a href="mailto:developers@coda.co">
           <img
             src="/codaco.png"
@@ -9,16 +9,15 @@ export default function Home() {
             className="w-full h-auto max-w-2xl"
           />
         </a>
+        <p>
+          <a href="https://github.com/complexdatacollective">
+            https://github.com/complexdatacollective
+          </a>
+        </p>
       </div>
       <footer className="text-center text-sm text-gray-500 shrink grow-0 p-10 gap-6 flex flex-col">
         <p>
-          <a
-            href="/codaco-bylaws.pdf"
-            className="font-bold hover:underline hover:underline-offset-4"
-          >
-            By-laws
-          </a>
-          .
+          <a href="/codaco-bylaws.pdf">By-laws</a>.
         </p>
         Copright © {new Date().getFullYear()} Complex Data Collective
       </footer>
